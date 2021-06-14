6aa0970090b0c15ad008c093b316330186fb5098
Stephen Carter <cartch@gmail.com>
2021-06-14 10:33:51 -0300

    install ejs, set as view engine and create views folder

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 
 const port = 3000;
 
-console.log('restarting');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
